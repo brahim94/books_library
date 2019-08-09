f1596c2f32b5470e0eec5f860814a390457a66c6
HimDev <brahim-ayad@hotmail.com>
2019-08-09 22:00:26 +0100

    uploading images to singleBook

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Book } from '../models/book.model';
 import { Subject } from 'rxjs/Subject';
-import * as firebase from 'firebase'
-
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot
 @Injectable()
 export class BooksService {
 
@@ -21,7 +21,7 @@ export class BooksService {
 
    getBooks() {
     firebase.database().ref('/books')
-      .on('value', (data) => {
+      .on('value', (data: DataSnapshot) => {
           this.books = data.val() ? data.val() : [];
           this.emitBooks();
         }
@@ -32,7 +32,7 @@ export class BooksService {
     return new Promise(
       (resolve, reject) => {
         firebase.database().ref('/books/' + id).once('value').then(
-          (data) => {
+          (data: DataSnapshot) => {
             resolve(data.val());
           }, (error) => {
             reject(error);
@@ -46,15 +46,49 @@ export class BooksService {
     this.saveBooks();
     this.emitBooks();
   }
-  removeBook(book: Book){
+  removeBook(book: Book) {
+    if(book.photo) {
+      const storageRef = firebase.storage().refFromURL(book.photo);
+      storageRef.delete().then(
+        () => {
+          console.log('Photo removed!');
+        },
+        (error) => {
+          console.log('Could not remove photo! : ' + error);
+        }
+      );
+    }
     const bookIndexToRemove = this.books.findIndex(
       (bookEl) => {
-        if(bookEl == book) {
+        if(bookEl === book) {
           return true;
         }
       }
     );
     this.books.splice(bookIndexToRemove, 1);
     this.saveBooks();
-  this.emitBooks();  }
+    this.emitBooks();
+}
+
+   uploadFile(file: File) {
+    return new Promise(
+      (resolve, reject) => {
+        const almostUniqueFileName = Date.now().toString();
+        const upload = firebase.storage().ref()
+          .child('images/' + almostUniqueFileName + file.name).put(file);
+        upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
+          () => {
+            console.log('Chargement…');
+          },
+          (error) => {
+            console.log('Erreur de chargement ! : ' + error);
+            reject();
+          },
+          () => {
+            resolve(upload.snapshot.ref.getDownloadURL());
+          }
+        );
+      }
+    );
+}
 }
